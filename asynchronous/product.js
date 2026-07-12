3e5db3014366793c1bde8ef327ee2bfec979856f
UmarAslaam <umaraslaam2001@gmail.com>
2026-07-12 16:31:38 +0500

    'productsSearchwithApiData'

--- asynchronous/product.js
+++ asynchronous/product.js
@@ -1,59 +1,65 @@
-let produclist = document.getElementById('productList');
+let productList = document.getElementById('productList');
 let searchInput = document.getElementById('searchInput');
 let searchBtn = document.getElementById('searchBtn');
 
+(async function () {
+   let data = await fetch('https://dummyjson.com/products?limit=194');
+   let result = await data.json();
+   let products = await result.products;
+  //  console.log(products);
 
-(async function() {
-  let data = await  fetch('https://dummyjson.com/products?limit=194');
-  let result = await data.json();
-  let products = await result.products;
-  console.log(products)
-
-  products.forEach((prd) => {
-    produclist.innerHTML += `
-     <div class="col-lg-4 my-3">
-        <div class="card" style="height: 28rem;">
-            <img src="${prd.thumbnail}" class="card-img-top" alt="..." height="200">
-            <div class="card-body">
-                <h5 class="card-title">${prd.title}</h5>
-                <p class="card-text">${prd.description}</p>
-                <a class="btn btn-primary">${prd.price}</a>
-               
-            </div>
-          </div>
-      </div>
+   products.forEach((prd) => {
+    productList.innerHTML += `
+    <div class="col-lg-4 my-2 d-flex">
+    <div class="card h-100 w-100">
+        <img src="${prd.thumbnail}" class="card-img-top product-img" alt="...">
+        <div class="card-body d-flex flex-column">
+            <h5 class="card-title">${prd.title}</h5>
+            <p class="card-text">
+                ${prd.description}
+            </p>
+            <a href="#" class="btn btn-primary mt-auto">
+                $ ${prd.price}
+            </a>
+        </div>
+    </div>
+    </div>
+ 
     `
-  });
-  function search() {
-    produclist.innerHTML = "";
+   });
+
+   function search() {
+    productList.innerHTML = "";
     let userSearchQuery = searchInput.value.toLowerCase();
-    let searchResult = products.filter((product)=>{
-      if (product.title.toLowerCase().includes(userSearchQuery) || product.description.toLowerCase().includes(userSearchQuery) || product.price === userSearchQuery ) {
+    // console.log(userSearchQuery);
+    let userSearchResult = products.filter((product)=>{
+      if (product.title.toLowerCase().includes(userSearchQuery) || product.description.toLowerCase().includes(userSearchQuery) ||   Math.floor(product.price) == userSearchQuery  ) {
         return product;
       }
     })
-     console.log(searchResult)
-     if (searchResult.length>0) {
-      searchResult.forEach((prd)=>{
-            produclist.innerHTML += `
-            <div class="col-lg-4 my-3">
-                <div class="card" style="height: 28rem;">
-                    <img src="${prd.thumbnail}" class="card-img-top" alt="..." height="200">
-                    <div class="card-body">
-                        <h5 class="card-title">${prd.title}</h5>
-                        <p class="card-text">${prd.description}</p>
-                        <a class="btn btn-primary">${prd.price}</a>
-                      
-                    </div>
-                  </div>
+    if (userSearchResult.length > 0) {
+      userSearchResult.forEach((prd)=>{
+        productList.innerHTML += `
+          <div class="col-lg-4 my-2 d-flex">
+          <div class="card h-100 w-100">
+              <img src="${prd.thumbnail}" class="card-img-top product-img" alt="...">
+              <div class="card-body d-flex flex-column">
+                  <h5 class="card-title">${prd.title}</h5>
+                  <p class="card-text">
+                      ${prd.description}
+                  </p>
+                  <a href="#" class="btn btn-primary mt-auto">
+                      $ ${prd.price}
+                  </a>
               </div>
-    `
-      }) 
-     }
-     else{
-            produclist.innerHTML += `<h1>No Products Found.</h1>`
-      
-     }
-  }
-  searchBtn.addEventListener('click',search)
-}) ()
+            </div>`
+      })  
+    }
+    else{
+       productList.innerHTML = `<h1>No Products Found.</h1>`
+    }
+    
+   }
+
+   searchBtn.addEventListener('click',search)
+}) ();
